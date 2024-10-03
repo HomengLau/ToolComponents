8ab1af147f643fbb88b109e1e326ea341ba15ebb
HomengLau <liuhq6@126.com>
2024-10-03 21:38:00 +0800

    添加gson 容错解析组件

--- ToolComponents/GsonFactory/build.gradle.kts
+++ ToolComponents/GsonFactory/build.gradle.kts
@@ -15,6 +15,15 @@ android {
     }
 
     buildTypes {
+        debug {
+            sourceSets{
+                getByName("main"){
+                    manifest.srcFile("src/debug/AndroidManifest.xml")
+                    res.srcDirs("src/debug/res")
+                    java.srcDirs(listOf("src/main/java","src/debug/java"))
+                }
+            }
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
@@ -34,6 +43,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.gson)
+    // Kotlin 反射库：用于反射 Kotlin data class 类对象，1.5.10 请修改成当前项目 Kotlin 的版本号
+    implementation(libs.kotlin.reflect)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
